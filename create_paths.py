a50dcadff9cac071efd52936a2fae152d7539e8e
haxul <s.s.starodubov@yandex.ru>
2020-08-14 21:40:16 +0400

    fix create_paths.py

--- create_paths.py
+++ create_paths.py
@@ -20,15 +20,20 @@ for i in args:
         path = i.split("=")[1]
         if path[-1] == "/":
             stop_script("<<<incorrect path>>>")
+        if path == "":
+            stop_script("<<<path is empty>>>")
     if "dirs" in i:
         dirs = i.split("=")[1].split(",")
+        if len(dirs) == 0:
+            stop_script("<<<dirs is empty>>>")
+
 
 if not path or not dirs:
-    stop_script("<<<wrong parameters>>>")
+    stop_script("<<<path or dirs equal None>>>")
 
 for dir_elem in dirs:
     new_dir = path + "/" + dir_elem
     if not os.path.exists(new_dir):
         os.makedirs(new_dir)
-        print("create new dir in " + new_dir + "\n")
+        print("create new dir in " + new_dir)
 
