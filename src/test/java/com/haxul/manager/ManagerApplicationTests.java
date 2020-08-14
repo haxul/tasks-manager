570e0adec7b0b5ba71d3e6d9cc989c47637a57e4
haxul <s.s.starodubov@yandex.ru>
2020-08-14 20:32:31 +0400

    init

---
+++ src/test/java/com/haxul/manager/ManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haxul.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
