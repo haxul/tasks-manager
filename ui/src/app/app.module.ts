92c52a66fe223b1b58c0511200864d7c49ad9a4f
haxul <s.s.starodubov@yandex.ru>
2020-08-18 17:53:21 +0400

    init ui

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,16 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {RouterModule} from '@angular/router';
+import {UnknownComponent} from './unknown/unknown.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UnknownComponent,
+    LoginComponent,
+    RegistrationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: "notfound", component: UnknownComponent},
+      {path: "signup", component: RegistrationComponent},
+      {path: "login", component: LoginComponent},
+      {path: '**', redirectTo: "notfound", pathMatch: 'full'}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
